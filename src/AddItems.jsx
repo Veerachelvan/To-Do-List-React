66f2bcc4a01724c363d6cb69a0b0dea35158e90b
Veerachelvan <veerachelvanmoorthy199@gmail.com>
2024-09-10 12:26:29 +0530

    Update AddItems.jsx

--- src/AddItems.jsx
+++ src/AddItems.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
+import { useRef } from 'react';
 
-const AddItems = ({addItems, setAddItems, handleAddItems}) => {
+
+const AddItems = ({ addItems, setAddItems, handleAddItems }) => {
+
+  const inputRef = useRef()
+
   return (
-    <form className="addItems" onSubmit={handleAddItems}> 
-        <label htmlFor="AddItems">Add Items</label>
-        <input 
-        type="text" 
+    <form className="addItems" onSubmit={handleAddItems}>
+      <label htmlFor="AddItems">Add Items</label>
+      <input
+        type="text"
+        ref={inputRef}
         placeholder='Add Items'
         required
         value={addItems}
-        onChange={(e)=>setAddItems(e.target.value)}
-        />
-        <button
+        onChange={(e) => setAddItems(e.target.value)}
+      />
+      <button
         type='submit'
-        >+
-        </button>
+        onClick={() => inputRef.current.focus()}
+      >+
+      </button>
     </form>
   )
 }
